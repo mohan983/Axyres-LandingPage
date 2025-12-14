91316514e1c4d1fb679940a792bd02e07bcb536c
mohan983 <jangamjagan123@gmail.com>
2025-12-14 18:43:44 +0530

    url update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 4000
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+  origin: 'https://axyres-landing-page.axyres.com',
+  methods: ['POST'],
+  credentials: true
+}));
 // Serve static files (for uploaded images)
 app.use('/uploads', express.static('uploads'))
 
